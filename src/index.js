4d1953b43d594173a3bbb9abbb3ce931049d57ee
Mohit Singhal <singhalmohit011@gmail.com>
2022-02-22 11:21:28 +0530

    project initialised

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const connect  = require("./configs/db");
+
+const app = express();
+app.listen(2345, async()=>{
+    try{
+        await connect();
+        console.log("listening on port 2345")
+    }
+    catch(err){
+        console.log(err.message);
+    }
+})
